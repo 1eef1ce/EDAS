8bfd598f847ae5294566f734536ac6caa0418655
1eef1ce <1eef1ce@gmail.com>
2019-11-15 15:43:12 +0500

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,19 @@
 $( document ).ready(function() {
+
+    new WOW().init();
+
     $(".navbar-toggler").on("click", function () {
         $(this).toggleClass("active");
     });
+
     $.each($('.navbar-nav').find('li'), function() {
         $(this).toggleClass('active',
             window.location.pathname.indexOf($(this).find('a').attr('href')) > -1);
     });
+
+    $('.link-vertical').click(function(e) {
+        e.preventDefault();
+        $(this).next('.list-hidden').show();
+    })
+
 });
